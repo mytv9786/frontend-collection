0aae1497003e72fe9bcf82d9214b5fcabf227875
saiteja1188 <loveui19@gmail.com>
2026-09-15 17:12:33 +0530

    Backend: connected to aiven mysql cloud

--- Backend/database/DbConnection.js
+++ Backend/database/DbConnection.js
@@ -1,13 +1,18 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 's123',
-  database: 'collection',
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 's123',
+  database: process.env.DB_NAME || 'collection',
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+  // 💡 Aiven క్లౌడ్ డేటాబేస్ కనెక్ట్ అవ్వడానికి SSL చాలా ముఖ్యం
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 pool
