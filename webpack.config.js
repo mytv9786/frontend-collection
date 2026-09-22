8bb57b12bdff0f2c83cd80cb828cd7376c915243
mytv9786 <mytv9786@gmail.com>
2026-09-22 18:08:24 +0530

    webpack change

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,229 @@
+// // const path = require('path');
+// // const webpack = require('webpack');
+// // const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// // const appDirectory = path.resolve(__dirname);
+
+// // module.exports = {
+// //   entry: ['regenerator-runtime/runtime', path.join(__dirname, 'index.web.js')],
+// //   devtool: 'source-map',
+// //   output: {
+// //     path: path.resolve(appDirectory, 'dist'),
+// //     publicPath: '',
+// //     filename: 'rnw_blogpost.bundle.js',
+// //   },
+// //   resolve: {
+// //     extensions: ['.tsx', '.js', '.ts', '.web.tsx', '.web.ts', '.web.js'],
+// //     alias: {
+// //       'react-native$': 'react-native-web',
+// //       'react-native-vector-icons/dist/MaterialCommunityIcons':
+// //         'react-native-web/dist/exports/View',
+// //       'react-native-vector-icons/MaterialCommunityIcons':
+// //         'react-native-web/dist/exports/View',
+// //       'react-native-vector-icons': 'react-native-web/dist/exports/View',
+// //       '@react-native-vector-icons/material-design-icons':
+// //         'react-native-web/dist/exports/View',
+// //     },
+// //   },
+// //   module: {
+// //     rules: [
+// //       {
+// //         test: /\.m?js$/,
+// //         resolve: {
+// //           fullySpecified: false,
+// //         },
+// //       },
+// //       {
+// //         test: /\.(js|jsx|ts|tsx)$/,
+// //         include: filePath => {
+// //           if (
+// //             filePath.includes(appDirectory) &&
+// //             !filePath.includes('node_modules')
+// //           ) {
+// //             return true;
+// //           }
+// //           if (
+// //             filePath.includes('node_modules/@react-navigation') ||
+// //             filePath.includes('node_modules/react-native') ||
+// //             filePath.includes('node_modules/@expo') ||
+// //             filePath.includes('node_modules/expo-') ||
+// //             filePath.includes('node_modules/react-native-vector-icons') ||
+// //             filePath.includes('node_modules/@expo/vector-icons') ||
+// //             filePath.includes('node_modules/@react-native-async-storage') ||
+// //             filePath.includes('node_modules/@react-native-picker/picker') ||
+// //             filePath.includes('node_modules/react-native-chart-kit') ||
+// //             filePath.includes('node_modules/react-native-svg') ||
+// //             filePath.includes('node_modules/lucide-react-native') ||
+// //             filePath.includes('node_modules/react-native-safe-area-context')
+// //           ) {
+// //             return true;
+// //           }
+// //           return false;
+// //         },
+// //         use: {
+// //           loader: 'babel-loader',
+// //           options: {
+// //             cacheDirectory: true,
+// //             presets: [
+// //               ['@babel/preset-env', { loose: true }],
+// //               '@babel/preset-react',
+// //               '@babel/preset-typescript',
+// //             ],
+// //             plugins: [
+// //               ['@babel/plugin-transform-class-properties', { loose: true }],
+// //               ['@babel/plugin-transform-private-methods', { loose: true }],
+// //               [
+// //                 '@babel/plugin-transform-private-property-in-object',
+// //                 { loose: true },
+// //               ],
+// //               'react-native-web',
+// //               '@babel/plugin-transform-modules-commonjs',
+// //             ],
+// //           },
+// //         },
+// //         resolve: {
+// //           fullySpecified: false,
+// //         },
+// //       },
+// //       {
+// //         test: /\.svg$/,
+// //         use: [{ loader: '@svgr/webpack' }],
+// //       },
+// //       {
+// //         test: /\.(gif|jpe?g|png)$/,
+// //         use: {
+// //           loader: 'url-loader',
+// //           options: {
+// //             name: '[name].[ext]',
+// //           },
+// //         },
+// //       },
+// //     ],
+// //   },
+// //   plugins: [
+// //     new HtmlWebpackPlugin({
+// //       template: path.join(__dirname, 'index.html'),
+// //     }),
+// //     new webpack.DefinePlugin({
+// //       __DEV__: JSON.stringify(true),
+// //     }),
+// //   ],
+// // };
+
+// // const path = require('path');
+// // const webpack = require('webpack');
+// // const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// // const appDirectory = path.resolve(__dirname);
+
+// // module.exports = {
+// //   entry: ['regenerator-runtime/runtime', path.join(__dirname, 'index.web.js')],
+// //   output: {
+// //     path: path.resolve(appDirectory, 'dist'),
+// //     publicPath: '',
+// //     filename: 'rnw_blogpost.bundle.js',
+// //   },
+// //   resolve: {
+// //     extensions: ['.tsx', '.web.tsx', '.web.ts', '.ts', '.web.js', '.js'],
+// //     alias: {
+// //       'react-native$': 'react-native-web',
+// //       // 💡 ఈ 4 లైన్స్ వెబ్ రన్‌టైమ్ క్రాష్ అవ్వకుండా ఐకాన్ ఇంపోర్ట్స్‌ను సేఫ్ గా రూట్ చేస్తాయి
+// //       'react-native-vector-icons/dist/MaterialCommunityIcons':
+// //         'react-native-web/dist/exports/View',
+// //       'react-native-vector-icons/MaterialCommunityIcons':
+// //         'react-native-web/dist/exports/View',
+// //       'react-native-vector-icons': 'react-native-web/dist/exports/View',
+// //       '@react-native-vector-icons/material-design-icons':
+// //         'react-native-web/dist/exports/View',
+// //     },
+// //   },
+// //   module: {
+// //     rules: [
+// //       {
+// //         test: /\.m?js$/,
+// //         resolve: {
+// //           fullySpecified: false,
+// //         },
+// //       },
+// //       {
+// //         test: /\.(js|jsx|ts|tsx)$/,
+// //         include: filePath => {
+// //           if (
+// //             filePath.includes(appDirectory) &&
+// //             !filePath.includes('node_modules')
+// //           ) {
+// //             return true;
+// //           }
+// //           if (
+// //             filePath.includes('node_modules/@react-navigation') ||
+// //             filePath.includes('node_modules/react-native') ||
+// //             filePath.includes('node_modules/@expo') ||
+// //             filePath.includes('node_modules/expo-') ||
+// //             filePath.includes('node_modules/react-native-vector-icons') ||
+// //             filePath.includes('node_modules/@expo/vector-icons') ||
+// //             filePath.includes('node_modules/@react-native-picker/picker') ||
+// //             filePath.includes('node_modules/react-native-chart-kit') ||
+// //             filePath.includes('node_modules/react-native-svg') ||
+// //             filePath.include(
+// //               'node_modules/@react-native-async-storage/async-storage',
+// //             ) ||
+// //             filePath.includes('node_modules/lucide-react-native')
+// //           ) {
+// //             return true;
+// //           }
+// //           return false;
+// //         },
+// //         use: {
+// //           loader: 'babel-loader',
+// //           options: {
+// //             cacheDirectory: true,
+// //             // 💡 ఇక్కడ రూట్ బాబెల్ ఫైల్ ప్రెసెట్స్ కాకుండా నేరుగా వెబ్ ప్రెసెట్స్ ఇచ్చాము
+// //             presets: [
+// //               ['@babel/preset-env', { loose: true }],
+// //               '@babel/preset-react',
+// //               '@babel/preset-typescript',
+// //             ],
+// //             plugins: [
+// //               ['@babel/plugin-transform-class-properties', { loose: true }],
+// //               ['@babel/plugin-transform-private-methods', { loose: true }],
+// //               [
+// //                 '@babel/plugin-transform-private-property-in-object',
+// //                 { loose: true },
+// //               ],
+// //               'react-native-web',
+// //               '@babel/plugin-transform-modules-commonjs',
+// //             ],
+// //           },
+// //         },
+// //         resolve: {
+// //           fullySpecified: false,
+// //         },
+// //       },
+// //       {
+// //         test: /\.svg$/,
+// //         use: [{ loader: '@svgr/webpack' }],
+// //       },
+// //       {
+// //         test: /\.(gif|jpe?g|png)$/,
+// //         use: {
+// //           loader: 'url-loader',
+// //           options: {
+// //             name: '[name].[ext]',
+// //           },
+// //         },
+// //       },
+// //     ],
+// //   },
+// //   plugins: [
+// //     new HtmlWebpackPlugin({
+// //       template: path.join(__dirname, 'index.html'),
+// //     }),
+// //     new webpack.DefinePlugin({
+// //       __DEV__: JSON.stringify(true),
+// //     }),
+// //   ],
+// // };
+
 // const path = require('path');
 // const webpack = require('webpack');
 // const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -6,14 +232,14 @@
 
 // module.exports = {
 //   entry: ['regenerator-runtime/runtime', path.join(__dirname, 'index.web.js')],
-//   devtool: 'source-map',
+//   devtool: 'source-map', // ← ఇది add చేయండి
 //   output: {
 //     path: path.resolve(appDirectory, 'dist'),
 //     publicPath: '',
 //     filename: 'rnw_blogpost.bundle.js',
 //   },
 //   resolve: {
-//     extensions: ['.tsx', '.js', '.ts', '.web.tsx', '.web.ts', '.web.js'],
+//     extensions: ['.tsx', '.web.tsx', '.web.ts', '.ts', '.web.js', '.js'],
 //     alias: {
 //       'react-native$': 'react-native-web',
 //       'react-native-vector-icons/dist/MaterialCommunityIcons':
@@ -53,120 +279,6 @@
 //             filePath.includes('node_modules/@react-native-picker/picker') ||
 //             filePath.includes('node_modules/react-native-chart-kit') ||
 //             filePath.includes('node_modules/react-native-svg') ||
-//             filePath.includes('node_modules/lucide-react-native') ||
-//             filePath.includes('node_modules/react-native-safe-area-context')
-//           ) {
-//             return true;
-//           }
-//           return false;
-//         },
-//         use: {
-//           loader: 'babel-loader',
-//           options: {
-//             cacheDirectory: true,
-//             presets: [
-//               ['@babel/preset-env', { loose: true }],
-//               '@babel/preset-react',
-//               '@babel/preset-typescript',
-//             ],
-//             plugins: [
-//               ['@babel/plugin-transform-class-properties', { loose: true }],
-//               ['@babel/plugin-transform-private-methods', { loose: true }],
-//               [
-//                 '@babel/plugin-transform-private-property-in-object',
-//                 { loose: true },
-//               ],
-//               'react-native-web',
-//               '@babel/plugin-transform-modules-commonjs',
-//             ],
-//           },
-//         },
-//         resolve: {
-//           fullySpecified: false,
-//         },
-//       },
-//       {
-//         test: /\.svg$/,
-//         use: [{ loader: '@svgr/webpack' }],
-//       },
-//       {
-//         test: /\.(gif|jpe?g|png)$/,
-//         use: {
-//           loader: 'url-loader',
-//           options: {
-//             name: '[name].[ext]',
-//           },
-//         },
-//       },
-//     ],
-//   },
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       template: path.join(__dirname, 'index.html'),
-//     }),
-//     new webpack.DefinePlugin({
-//       __DEV__: JSON.stringify(true),
-//     }),
-//   ],
-// };
-
-// const path = require('path');
-// const webpack = require('webpack');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-// const appDirectory = path.resolve(__dirname);
-
-// module.exports = {
-//   entry: ['regenerator-runtime/runtime', path.join(__dirname, 'index.web.js')],
-//   output: {
-//     path: path.resolve(appDirectory, 'dist'),
-//     publicPath: '',
-//     filename: 'rnw_blogpost.bundle.js',
-//   },
-//   resolve: {
-//     extensions: ['.tsx', '.web.tsx', '.web.ts', '.ts', '.web.js', '.js'],
-//     alias: {
-//       'react-native$': 'react-native-web',
-//       // 💡 ఈ 4 లైన్స్ వెబ్ రన్‌టైమ్ క్రాష్ అవ్వకుండా ఐకాన్ ఇంపోర్ట్స్‌ను సేఫ్ గా రూట్ చేస్తాయి
-//       'react-native-vector-icons/dist/MaterialCommunityIcons':
-//         'react-native-web/dist/exports/View',
-//       'react-native-vector-icons/MaterialCommunityIcons':
-//         'react-native-web/dist/exports/View',
-//       'react-native-vector-icons': 'react-native-web/dist/exports/View',
-//       '@react-native-vector-icons/material-design-icons':
-//         'react-native-web/dist/exports/View',
-//     },
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.m?js$/,
-//         resolve: {
-//           fullySpecified: false,
-//         },
-//       },
-//       {
-//         test: /\.(js|jsx|ts|tsx)$/,
-//         include: filePath => {
-//           if (
-//             filePath.includes(appDirectory) &&
-//             !filePath.includes('node_modules')
-//           ) {
-//             return true;
-//           }
-//           if (
-//             filePath.includes('node_modules/@react-navigation') ||
-//             filePath.includes('node_modules/react-native') ||
-//             filePath.includes('node_modules/@expo') ||
-//             filePath.includes('node_modules/expo-') ||
-//             filePath.includes('node_modules/react-native-vector-icons') ||
-//             filePath.includes('node_modules/@expo/vector-icons') ||
-//             filePath.includes('node_modules/@react-native-picker/picker') ||
-//             filePath.includes('node_modules/react-native-chart-kit') ||
-//             filePath.includes('node_modules/react-native-svg') ||
-//             filePath.include(
-//               'node_modules/@react-native-async-storage/async-storage',
-//             ) ||
 //             filePath.includes('node_modules/lucide-react-native')
 //           ) {
 //             return true;
@@ -177,9 +289,8 @@
 //           loader: 'babel-loader',
 //           options: {
 //             cacheDirectory: true,
-//             // 💡 ఇక్కడ రూట్ బాబెల్ ఫైల్ ప్రెసెట్స్ కాకుండా నేరుగా వెబ్ ప్రెసెట్స్ ఇచ్చాము
 //             presets: [
-//               ['@babel/preset-env', { loose: true }],
+//               ['@babel/preset-env', { loose: true, modules: false }],
 //               '@babel/preset-react',
 //               '@babel/preset-typescript',
 //             ],
@@ -191,7 +302,7 @@
 //                 { loose: true },
 //               ],
 //               'react-native-web',
-//               '@babel/plugin-transform-modules-commonjs',
+//               // 💡 ఇక్కడి నుండి CommonJS ప్లగిన్‌ను పూర్తిగా తొలగించాము
 //             ],
 //           },
 //         },
@@ -219,7 +330,8 @@
 //       template: path.join(__dirname, 'index.html'),
 //     }),
 //     new webpack.DefinePlugin({
-//       __DEV__: JSON.stringify(true),
+//       // 💡 ఇది బిల్డ్ మోడ్‌ని బట్టి DEV ఎన్విరాన్‌మెంట్‌ను ఆటోమేటిక్‌గా సెట్ చేస్తుంది
+//       __DEV__: process.env.NODE_ENV !== 'production',
 //     }),
 //   ],
 // };
@@ -231,15 +343,17 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const appDirectory = path.resolve(__dirname);
 
 module.exports = {
+  // ఎంట్రీ పాయింట్‌ను index.web.js కి కరెక్ట్‌గా సెట్ చేసాము
   entry: ['regenerator-runtime/runtime', path.join(__dirname, 'index.web.js')],
-  devtool: 'source-map', // ← ఇది add చేయండి
+  devtool: 'source-map',
   output: {
     path: path.resolve(appDirectory, 'dist'),
     publicPath: '',
     filename: 'rnw_blogpost.bundle.js',
+    clean: true, // ప్రతి బిల్డ్ లోనూ పాత ఫైల్స్ క్లీన్ చేస్తుంది
   },
   resolve: {
-    extensions: ['.tsx', '.web.tsx', '.web.ts', '.ts', '.web.js', '.js'],
+    extensions: ['.web.tsx', '.web.ts', '.web.js', '.tsx', '.ts', '.js'],
     alias: {
       'react-native$': 'react-native-web',
       'react-native-vector-icons/dist/MaterialCommunityIcons':
@@ -255,9 +369,7 @@ module.exports = {
     rules: [
       {
         test: /\.m?js$/,
-        resolve: {
-          fullySpecified: false,
-        },
+        resolve: { fullySpecified: false },
       },
       {
         test: /\.(js|jsx|ts|tsx)$/,
@@ -279,7 +391,8 @@ module.exports = {
             filePath.includes('node_modules/@react-native-picker/picker') ||
             filePath.includes('node_modules/react-native-chart-kit') ||
             filePath.includes('node_modules/react-native-svg') ||
-            filePath.includes('node_modules/lucide-react-native')
+            filePath.includes('node_modules/lucide-react-native') ||
+            filePath.includes('node_modules/react-native-safe-area-context')
           ) {
             return true;
           }
@@ -290,7 +403,7 @@ module.exports = {
           options: {
             cacheDirectory: true,
             presets: [
-              ['@babel/preset-env', { loose: true, modules: false }],
+              ['@babel/preset-env', { loose: true }],
               '@babel/preset-react',
               '@babel/preset-typescript',
             ],
@@ -302,13 +415,11 @@ module.exports = {
                 { loose: true },
               ],
               'react-native-web',
-              // 💡 ఇక్కడి నుండి CommonJS ప్లగిన్‌ను పూర్తిగా తొలగించాము
+              '@babel/plugin-transform-modules-commonjs',
             ],
           },
         },
-        resolve: {
-          fullySpecified: false,
-        },
+        resolve: { fullySpecified: false },
       },
       {
         test: /\.svg$/,
@@ -318,9 +429,7 @@ module.exports = {
         test: /\.(gif|jpe?g|png)$/,
         use: {
           loader: 'url-loader',
-          options: {
-            name: '[name].[ext]',
-          },
+          options: { name: '[name].[ext]' },
         },
       },
     ],
@@ -330,8 +439,8 @@ module.exports = {
       template: path.join(__dirname, 'index.html'),
     }),
     new webpack.DefinePlugin({
-      // 💡 ఇది బిల్డ్ మోడ్‌ని బట్టి DEV ఎన్విరాన్‌మెంట్‌ను ఆటోమేటిక్‌గా సెట్ చేస్తుంది
-      __DEV__: process.env.NODE_ENV !== 'production',
+      // 🟢 ప్రొడక్షన్ మోడ్ లో క్రాష్ అవ్వకుండా ఎన్విరాన్మెంట్ కి తగ్గట్టు DEV మారుతుంది
+      __DEV__: JSON.stringify(process.env.NODE_ENV !== 'production'),
     }),
   ],
 };
