22eeecd82c04925d08bc7db90def77314bb15a72
mytv9786 <mytv9786@gmail.com>
2026-09-22 08:32:12 +0530

    change index.js

--- index.js
+++ index.js
@@ -4,10 +4,10 @@
 
 import { AppRegistry } from 'react-native';
 import App from './App';
-//import { name as appName } from './app.json';
+import { name as appName } from './app.json';
 
 //AppRegistry.registerComponent(appName, () => App);
 
 // ఈ రెండు లైన్లను యాడ్ చేయండి. ఏ పేరుతో అడిగినా యాప్ ఓపెన్ అవుతుంది.
-AppRegistry.registerComponent('RNCProject', () => App);
-AppRegistry.registerComponent('rncproject', () => App);
+AppRegistry.registerComponent(appName, () => App);
+//AppRegistry.registerComponent('rncproject', () => App);
