2e046a4b592fc17ecb5779d34367dffcada2879f
mytv9786 <mytv9786@gmail.com>
2026-09-22 18:03:08 +0530

    change index.js

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-/**
- * @format
- */
+// index.js లోపల కూడా ఈ అల్టిమేట్ గార్డ్ పెట్టండి
+(function () {
+  const safeWrap = originalFn => {
+    if (typeof originalFn !== 'function') return originalFn;
+    return function (obj) {
+      if (obj === null || obj === undefined) return [];
+      try {
+        return originalFn(obj);
+      } catch (e) {
+        return [];
+      }
+    };
+  };
+  Object.keys = safeWrap(Object.keys);
+  Object.values = safeWrap(Object.values);
+  Object.entries = safeWrap(Object.entries);
+})();
 
 import { AppRegistry } from 'react-native';
 import App from './App';
-import { name as appName } from './app.json';
+import appConfig from './app.json';
+const appName = appConfig.name;
 
-//AppRegistry.registerComponent(appName, () => App);
-
-// ఈ రెండు లైన్లను యాడ్ చేయండి. ఏ పేరుతో అడిగినా యాప్ ఓపెన్ అవుతుంది.
 AppRegistry.registerComponent(appName, () => App);
-//AppRegistry.registerComponent('rncproject', () => App);
