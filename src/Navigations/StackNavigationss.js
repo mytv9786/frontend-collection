5ed21c5182729774a488adaabab50e4788f26436
mytv9786 <mytv9786@gmail.com>
2026-09-22 16:48:37 +0530

    cratestacknavigarot

--- src/Navigations/StackNavigationss.js
+++ src/Navigations/StackNavigationss.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// ❌ పాత native-stack ఇంపోర్ట్ తీసేశాము
+// 🟢 కొత్త స్టాండర్డ్ stack ఇంపోర్ట్ యాడ్ చేశాము
+import { createStackNavigator } from '@react-navigation/stack';
 
 import LoginScreen from '../Screens/LoginScreen';
 
-const Stack = createNativeStackNavigator();
+// Native Stack కి బదులుగా నార్మల్ Stack వాడాలి
+const Stack = createStackNavigator();
 
-const StackNavigation = () => {
+const StackNavigationss = () => {
   return (
     <Stack.Navigator
       screenOptions={{ headerShown: false }}
@@ -16,4 +19,4 @@ const StackNavigation = () => {
   );
 };
 
-export default StackNavigation;
+export default StackNavigationss;
