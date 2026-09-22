dadb820c541c37951644c1b70a58d0b192b43708
mytv9786 <mytv9786@gmail.com>
2026-09-22 17:56:29 +0530

    move safety guard to the absolute top of index web

--- index.web.js
+++ index.web.js
@@ -1,36 +1,16 @@
-// import 'regenerator-runtime/runtime';
-// import { AppRegistry } from 'react-native';
-// //import { name as appName } from './app.json';
-// import App from './App';
-// import appConfig from './app.json';
-// const appName = appConfig.name;
-
-// /*
-// if (module.hot) {
-//   module.hot.accept();
-// }
-// */
-
-// AppRegistry.registerComponent(appName, () => App);
-// AppRegistry.runApplication(appName, {
-//   initialProps: {},
-//   rootTag:
-//     document.querySelector('#app-root') || document.getElementById('app-root'),
-// });
-
-import 'regenerator-runtime/runtime';
-
-// 1. 🟢 ప్రొడక్షన్ మోడ్‌లో వచ్చే Object.keys() క్రాష్‌ను ఆపడానికి గ్లోబల్ సేఫ్టీ గార్డ్
+// 1. 🟢 అందరికంటే ముందే, ఫస్ట్ లైన్ లోనే ఈ సేఫ్టీ గార్డ్ ఉండాలి (ఇంపోర్ట్స్ కంటే ముందే!)
 if (typeof Object.keys === 'function') {
   const originalKeys = Object.keys;
   Object.keys = function (obj) {
     if (obj === null || obj === undefined) {
-      return []; // క్రాష్ అవ్వకుండా ఖాళీ అర్రేను పంపుతుంది
+      return [];
     }
     return originalKeys(obj);
   };
 }
 
+// 2. దీని తర్వాతే మీ మిగిలిన పాత ఇంపోర్ట్స్ అన్నీ రావాలి
+import 'regenerator-runtime/runtime';
 import { AppRegistry } from 'react-native';
 import App from './App';
 import appConfig from './app.json';
@@ -38,7 +18,6 @@ const appName = appConfig.name;
 
 AppRegistry.registerComponent(appName, () => App);
 
-// 2. 🟢 ఎలిమెంట్ ఉందో లేదో సేఫ్‌గా చెక్ చేసి అప్లికేషన్‌ను రన్ చేస్తాము
 const rootElement =
   document.querySelector('#app-root') || document.getElementById('app-root');
 
